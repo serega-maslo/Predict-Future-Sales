374fde0d81d6e0e6d029b594b17140d0b728f115
serega-maslo <paulobelov228@gmail.com>
2026-07-16 13:57:19 +0300

    plot_functions + class-based etl + etl_validation notebook
    Transfered plot functions from eda notebook to separate script. Simplified etl by separating each table loader and using classes. Add notebook for testing etl.

--- src/data/etl.py
+++ src/data/etl.py
@@ -1,4 +1,3 @@
-import math
 import re
 import sys
 from pathlib import Path
@@ -10,33 +9,12 @@ project_root = Path(__file__).resolve().parents[2]
 if str(project_root) not in sys.path:
     sys.path.append(str(project_root))
 
+from src.data.constants import INFLATION_DATA
+
 raw_data_path = project_root / "data" / "raw"
 cache_path = project_root / "data" / "processed"
 cache_path.mkdir(parents=True, exist_ok=True)
 
-'''from src.data.constants import (
-    CLUSTERS_OF_ITEMS,
-    INFLATION_DATA,
-    NUMBER_OF_DAYS_IN_MONTH,
-    NUMBER_OF_DAYS_OF_MONTH,
-    NUMBER_OF_DAYS_OF_WEEK,
-    POPULATION_DATA,
-)'''
-
-
-
-def _cache_file(name: str) -> Path:
-    return cache_path / f"{name}.csv"
-
-def _load_or_compute(cache_name, recompute, compute_fn):
-    cache = _cache_file(cache_name)
-
-    if cache.exists() and not recompute:
-        return pd.read_csv(cache)
-
-    df = compute_fn()
-    df.to_csv(cache, index=False)
-    return df
 
 
 def _get_main_category(item_category_name):
@@ -53,6 +31,19 @@ def _compute_item_cats(raw_data_path: Path) -> pd.DataFrame:
     item_cats = pd.read_csv(raw_data_path / "item_categories.csv")
     item_cats["main_category"] = item_cats["item_category_name"].map(_get_main_category)
     return item_cats
+
+
+def _clean_shop_name(text):
+    return text.lstrip('!')
+
+
+def _get_shop_city(text):
+    city = text.split()[0]
+    if city == 'Выездная':
+        city = 'Выездная торговля'
+    elif city == 'Цифровой' or city == 'Интернет-магазин':
+        city = 'Интернет'
+    return city
 
 
 
@@ -72,277 +63,150 @@ def _compute_items(raw_data_path: Path) -> pd.DataFrame:
 
 
 
-def load_item_cats(
-    raw_data_path=raw_data_path,
-    recompute=True,
-):
-    return _load_or_compute(
-        cache_name="item_cats_processed",
-        recompute=recompute,
-        compute_fn=lambda: _compute_item_cats(raw_data_path),
-    ) 
+def _compute_shops(raw_data_path: Path) -> pd.DataFrame:
+    shops = pd.read_csv(raw_data_path / "shops.csv")
+    shops['shop_name'] = shops["shop_name"].apply(_clean_shop_name)
+    shops['city'] = shops['shop_name'].apply(_get_shop_city)
+    shops['corrected_shop_id'] = shops['shop_id']
+    shops.loc[shops['shop_name'] == 'Якутск Орджоникидзе, 56 фран', ['corrected_shop_id']] = [shops[shops["shop_name"] == 'Якутск Орджоникидзе, 56'].iloc[0]['shop_id']]
+    shops.loc[shops['shop_name'] == 'Якутск ТЦ "Центральный" фран', ['corrected_shop_id']] = [shops[shops["shop_name"] == 'Якутск ТЦ "Центральный"'].iloc[0]['shop_id']]
+    shops.loc[shops['shop_name'] == 'Жуковский ул. Чкалова 39м?', ['corrected_shop_id']] = [shops[shops["shop_name"] == 'Жуковский ул. Чкалова 39м²'].iloc[0]['shop_id']]
+    return shops
 
 
-def load_items(
-    raw_data_path=raw_data_path,
-    recompute=True,
-):
-    return _load_or_compute(
-        cache_name="shops_processed",
-        recompute=recompute,
-        compute_fn=lambda: _compute_items(raw_data_path),
-    ) 
-
-if __name__ == "__main__":
-    data = load_items(recompute=True)
-    print(data.head())
-
-
-
-
-
-'''
-def load_train(
-    raw_data_path=raw_data_path,
-    recompute=True,
-    split="all",          # "all", "train", "val", "test"
-    val_block=32,
-    test_block=33,
-):
-    def compute():
-
-        train = pd.read_csv(raw_data_path / "sales_train.csv")
-        test = pd.read_csv(raw_data_path / "test.csv")
-        items = pd.read_csv(raw_data_path / "items.csv")
-        item_cats = pd.read_csv(raw_data_path / "item_categories.csv")
-        shops = pd.read_csv(raw_data_path / "shops.csv")
+def _compute_train(raw_data_path: Path) -> pd.DataFrame:
+    train = pd.read_csv(raw_data_path / "sales_train.csv")
+    train = train.drop_duplicates(keep='first')
+    train['date'] = pd.to_datetime(train['date'], format='%d.%m.%Y')
+    train["year"] = train["date"].dt.year
+    train["month"] = train["date"].dt.month
+    train["day"] = train["date"].dt.day
+    train["week_day"] = train["date"].dt.day_of_week
+    train["month_day_sin"] = np.sin(2 * np.pi * train["day"] / train["date"].dt.days_in_month)
+    train["month_day_cos"] = np.cos(2 * np.pi * train["day"] / train["date"].dt.days_in_month)
+    train["month_sin"] = np.sin(2 * np.pi * train["month"] / 12)
+    train["month_cos"] = np.cos(2 * np.pi * train["month"] / 12)
+    train["week_day_sin"] = np.sin(2 * np.pi * train["week_day"] / 7)
+    train["week_day_cos"] = np.cos(2 * np.pi * train["week_day"] / 7)
+    train['inflation_factor'] = train.set_index(['year', 'month']).index.map(INFLATION_DATA)
+    train['no_inflation_price'] = train['item_price'] / train['inflation_factor']
+    train.drop(columns=['inflation_factor'], inplace=True)
+    return train
 
 
 
-        train = train.drop_duplicates(keep='first')
-        train['date'] = pd.to_datetime(train['date'], format='%d.%m.%Y')
-        train["year"] = train["date"].dt.year
-        train["month"] = train["date"].dt.month
-        train["day"] = train["date"].dt.day
-        train["week_day"] = train["date"].dt.day_of_week
-        train["month_day_sin"] = np.sin(2 * np.pi * train["day"] / train["date"].dt.days_in_month)
-        train["month_day_cos"] = np.cos(2 * np.pi * train["day"] / train["date"].dt.days_in_month)
-        train["month_sin"] = np.sin(2 * np.pi * train["month"] / 12)
-        train["month_cos"] = np.cos(2 * np.pi * train["month"] / 12)
-        train["week_day_sin"] = np.sin(2 * np.pi * train["week_day"] / 7)
-        train["week_day_cos"] = np.cos(2 * np.pi * train["week_day"] / 7)
+def _compute_df_final(raw_data_path: Path) -> pd.DataFrame:
+    shops = shops_loader().load()
+    items = items_loader().load()
+    item_cats = item_category_loader().load()
+    train = train_loader().load()
 
-
-        train['inflation_factor'] = train.set_index(['year', 'month']).index.map(INFLATION_DATA)
-        train['no_inflation_price'] = train['item_price'] / train['inflation_factor']
-
-
-        def clean_name(text):
-            text = re.sub(r'^[\!\*\/\s]+', '', text)
-            text = re.sub(r'[\!\*\/\s]$', '', text, flags=re.IGNORECASE)
-            return text.strip()
-        items['item_name'] = items['item_name'].apply(clean_name)
-        sorted(items["item_name"].unique())
-
-
-        items['item_name'] = items['item_name'].str.replace(r'\s+D$', ' Disc', regex=True, case=False)
-        sorted(items[items['item_name'].str.contains(r' +D$', case=True, na=False)]["item_name"].unique())
-
-        items['cleaned_name'] = (
-            items['item_name']
-            .astype(str)
-            .str.replace(r'[^a-zA-Zа-яА-ЯёЁ0-9]', '', regex=True)
-            .str.lower()
-        )
-
-        grouped = items.groupby('cleaned_name')['item_name'].apply(list)
-
-        duplicates = grouped[grouped.apply(len) > 1]
-        corrected_item_id_map = {}
-
-        for _, original_list in duplicates.items():
-            first_item = original_list[0]
-            first_id = items[items["item_name"] == first_item].iloc[0]["item_id"]
-            for item_name in original_list:
-                item_ids = items[items["item_name"] == item_name]["item_id"].tolist()
-                for item_id in item_ids:
-                    corrected_item_id_map[item_id] = first_id
-                items.loc[items['item_name'] == item_name, 'item_name'] = first_item
-        
-
-        train["item_id"] = train["item_id"].replace(corrected_item_id_map)
-
-
-        group_cols = train.columns.drop('item_cnt_day').tolist()
-        train = train.groupby(group_cols, as_index=False)['item_cnt_day'].sum()
-
-
-        split_cats = item_cats['item_category_name'].str.split('-', n=1, expand=True)
-
-        item_cats['main_category'] = split_cats[0].str.strip()
-
-        if split_cats.shape[1] > 1:
-            item_cats['subcategory'] = split_cats[1].str.strip()
-            item_cats['subcategory'] = item_cats['subcategory'].fillna(item_cats['main_category'])
-        else:
-            item_cats['subcategory'] = item_cats['main_category']
-
-        item_cats[['item_category_name', 'main_category', 'subcategory']].head(15)
-        item_cats["cluster"] = (
-        item_cats["item_category_name"]
-        .map(CLUSTERS_OF_ITEMS)
-        .fillna(9)
-        .astype(int)
-)
-
-
-        shops['shop_name'] = shops['shop_name'].str.lstrip('!')
-        shops["city"] = shops["shop_name"].str.split().str[0]
-        shops['city'] = shops['city'].replace({
-            'Выездная': 'Выездная торговля',
-            'Цифровой': 'Интернет',
-            'Интернет-магазин': 'Интернет'
-        })
-
-        corrected_shop_id_map = {}
-        corrected_shop_id_map[shops[shops["shop_name"] == 'Якутск Орджоникидзе, 56 фран'].iloc[0]["shop_id"]] = shops[shops["shop_name"] == 'Якутск Орджоникидзе, 56'].iloc[0]["shop_id"]
-        corrected_shop_id_map[shops[shops["shop_name"] == 'Якутск ТЦ "Центральный" фран'].iloc[0]["shop_id"]] = shops[shops["shop_name"] == 'Якутск ТЦ "Центральный"'].iloc[0]["shop_id"]
-        corrected_shop_id_map[shops[shops["shop_name"] == 'Жуковский ул. Чкалова 39м?'].iloc[0]["shop_id"]] = shops[shops["shop_name"] == 'Жуковский ул. Чкалова 39м²'].iloc[0]["shop_id"]
-        train["shop_id"] = train["shop_id"].replace(corrected_shop_id_map)
-        test["shop_id"] = test["shop_id"].replace(corrected_shop_id_map)
-
-
-        group_cols = train.columns.drop('item_cnt_day').tolist()
-        train = train.groupby(group_cols, as_index=False)['item_cnt_day'].sum()
-
-        items_completed = pd.merge(items, item_cats, on="item_category_id", how="left")
-        train_merged = pd.merge(train, items_completed, on="item_id", how="left")
-        df_final = pd.merge(train_merged, shops, on="shop_id", how="left")
-
-        df_final.loc[484683, "item_price"] = 1249.0
-        df_final.loc[484683, "no_inflation_price"] = 1249.0 / INFLATION_DATA[2013, 5]
-
-        df_final.loc[df_final['item_name'] == 'Radmin 3  - 522 лиц.', ['item_name', 'item_price', 'item_cnt_day', 'no_inflation_price']] = ["Radmin 3", 307980.0 / 522, 522, 289183.0985915493 / 522]
-
-
-
-        indices_to_drop = df_final[df_final["item_name"] == "Доставка (EMS)"].nlargest(1, "item_price").index
-        df_final = df_final.drop(indices_to_drop)
-
-
-        df_final.drop([2909818, 2864235, 2851091, 2608040, 2626181, 2851073, 2067669, 2864260], inplace=True)
-
-        indices_to_drop = df_final[df_final["item_name"] == "Доставка до пункта выдачи (Boxberry)"].nlargest(2, "item_cnt_day").index
-        df_final = df_final.drop(indices_to_drop)
-        
-
-        shop_speciality_sales = (
-            df_final.groupby(['shop_name', 'main_category'])['item_cnt_day']
-            .sum()
-            .reset_index()
-        ) 
-        shop_speciality_sales = shop_speciality_sales.sort_values(['shop_name', 'item_cnt_day'], ascending=[True, False])
-        shop_speciality_sales['rank'] = shop_speciality_sales.groupby('shop_name').cumcount() + 1
-        top_category = shop_speciality_sales[shop_speciality_sales['rank'] <= 1]
-        shop_specs = (
-            top_category.pivot(index='shop_name', columns='rank', values='main_category')
-            .rename(columns={1: 'shop_speciality'})
-            .fillna('Нет')
-        )
-        df_final = df_final.merge(shop_specs, on='shop_name', how='left')
-        columns_to_drop = ['date', 'month', 'day', 'week_day', 'inflation_factor', 'item_name', 'cleaned_name', 'item_category_name', 'item_category_id', 'shop_name', 'main_category', 'subcategory']
-        df_final.drop(columns=columns_to_drop, inplace=True)
-        return df_final
-    
-    df = _load_or_compute(
-        cache_name="train_processed",
-        recompute=recompute,
-        compute_fn=compute,
+    df_final = (
+    train
+    .merge(
+        items[
+            ["item_id", "corrected_item_id", "item_category_id", "item_name"]
+        ],
+        on="item_id",
+        how="left"
     )
+    .merge(
+        item_cats[
+            ["item_category_id", "main_category"]
+        ],
+        on="item_category_id",
+        how="left"
+    )
+    .merge(
+        shops[
+            ["shop_id", "corrected_shop_id", "city"]
+        ],
+        on="shop_id",
+        how="left"
+    )
+    .drop(columns=["item_category_id", "shop_id", "item_id"])
+    )
+    df_final.loc[484683, ["item_price", "no_inflation_price"]] = [1249.0, 1249.0 / INFLATION_DATA[2013, 5]]
+    df_final.loc[1256153, ['item_price', 'item_cnt_day', 'no_inflation_price']] = [307980.0 / 522, 522, 289183.0985915493 / 522]
+    indices_to_drop = df_final[df_final["item_name"] == "Доставка (EMS)"].nlargest(1, "item_price").index
+    df_final = df_final.drop(indices_to_drop)
+    df_final.drop([2909818, 2864235, 2851091, 2608040, 2626181, 2851073, 2067669, 2864260], inplace=True)
+    indices_to_drop = df_final[df_final["item_name"] == "Доставка до пункта выдачи (Boxberry)"].nlargest(2, "item_cnt_day").index
+    df_final = df_final.drop(indices_to_drop)
+    columns_to_drop = ['date', 'month', 'day', 'week_day', 'item_name']
+    df_final.drop(columns=columns_to_drop, inplace=True)
+    return df_final
 
-    if split == "train":
-        return df[df["date_block_num"] < val_block].reset_index(drop=True)
 
-    elif split == "val":
-        return df[df["date_block_num"] == val_block].reset_index(drop=True)
 
-    elif split == "test":
-        return df[df["date_block_num"] == test_block].reset_index(drop=True)
 
-    elif split == "all":
+def _compute_for_submission(raw_data_path: Path) -> pd.DataFrame:
+    for_submission = pd.read_csv(raw_data_path / "test.csv")
+    shops = shops_loader().load()
+    items = items_loader().load()
+    for_submission = (
+    for_submission
+    .merge(
+        items[
+            ["item_id", "corrected_item_id"]
+        ],
+        on="item_id",
+        how="left"
+    )
+    .merge(
+        shops[
+            ["shop_id", "corrected_shop_id"]
+        ],
+        on="shop_id",
+        how="left"
+    )
+    )
+    return for_submission
+
+
+
+class base_loader:
+    cache_name: str
+    compute_fn: callable
+
+
+    def __init__(self, raw_data_path: Path = raw_data_path, cache_path: Path = cache_path):
+        self.raw_data_path = raw_data_path
+        self.cache_path = cache_path
+
+    @property
+    def cache_file(self):
+        return self.cache_path / f"{self.cache_name}.csv"
+
+    def load(self, recompute=True) -> pd.DataFrame:
+        if self.cache_file.exists() and not recompute:
+            return pd.read_csv(self.cache_file)
+
+        df = self.compute_fn(self.raw_data_path)
+        df.to_csv(self.cache_file, index=False)
         return df
-
-    else:
-        raise ValueError(
-            "split must be one of {'all', 'train', 'val', 'test'}"
-        )
-
-
-
-
-def load_for_submission(
-        raw_data_path=raw_data_path,
-        recompute=True,
-):
-    def compute():
-        for_submission = pd.read_csv(raw_data_path / "test.csv")
-        items = pd.read_csv(raw_data_path / "items.csv")
-        shops = pd.read_csv(raw_data_path / "shops.csv")
-
-
-
-        def clean_name(text):
-            text = re.sub(r'^[\!\*\/\s]+', '', text)
-            text = re.sub(r'[\!\*\/\s]$', '', text, flags=re.IGNORECASE)
-            return text.strip()
-        items['item_name'] = items['item_name'].apply(clean_name)
-        sorted(items["item_name"].unique())
-
-
-        items['item_name'] = items['item_name'].str.replace(r'\s+D$', ' Disc', regex=True, case=False)
-        sorted(items[items['item_name'].str.contains(r' +D$', case=True, na=False)]["item_name"].unique())
-
-
-
-        items['cleaned_name'] = (
-            items['item_name']
-            .astype(str)
-            .str.replace(r'[^a-zA-Zа-яА-ЯёЁ0-9]', '', regex=True)
-            .str.lower()
-        )
-
-        grouped = items.groupby('cleaned_name')['item_name'].apply(list)
-
-        duplicates = grouped[grouped.apply(len) > 1]
-        corrected_item_id_map = {}
-
-        for _, original_list in duplicates.items():
-            first_item = original_list[0]
-            first_id = items[items["item_name"] == first_item].iloc[0]["item_id"]
-            for item_name in original_list:
-                item_ids = items[items["item_name"] == item_name]["item_id"].tolist()
-                for item_id in item_ids:
-                    corrected_item_id_map[item_id] = first_id
-                items.loc[items['item_name'] == item_name, 'item_name'] = first_item
-        
-        for_submission["item_id"] = for_submission["item_id"].replace(corrected_item_id_map)
-
-
-        shops['shop_name'] = shops['shop_name'].str.lstrip('!')
-        corrected_shop_id_map = {}
-        corrected_shop_id_map[shops[shops["shop_name"] == 'Якутск Орджоникидзе, 56 фран'].iloc[0]["shop_id"]] = shops[shops["shop_name"] == 'Якутск Орджоникидзе, 56'].iloc[0]["shop_id"]
-        corrected_shop_id_map[shops[shops["shop_name"] == 'Якутск ТЦ "Центральный" фран'].iloc[0]["shop_id"]] = shops[shops["shop_name"] == 'Якутск ТЦ "Центральный"'].iloc[0]["shop_id"]
-        corrected_shop_id_map[shops[shops["shop_name"] == 'Жуковский ул. Чкалова 39м?'].iloc[0]["shop_id"]] = shops[shops["shop_name"] == 'Жуковский ул. Чкалова 39м²'].iloc[0]["shop_id"]
-        for_submission["shop_id"] = for_submission["shop_id"].replace(corrected_shop_id_map)
-        return for_submission
-
-
     
-    return _load_or_compute(
-        cache_name="for_submission_processed",
-        recompute=recompute,
-        compute_fn=compute,
-    )
 
+class item_category_loader(base_loader):
+    cache_name = "item_cats_processed"
+    compute_fn = staticmethod(_compute_item_cats)
 
-'''
+class items_loader(base_loader):
+    cache_name = "items_processed"
+    compute_fn = staticmethod(_compute_items)
+
+class shops_loader(base_loader):
+    cache_name = "shops_processed"
+    compute_fn = staticmethod(_compute_shops)
+
+class train_loader(base_loader):
+    cache_name = "train_processed"
+    compute_fn = staticmethod(_compute_train)
+
+class df_final_loader(base_loader):
+    cache_name = "df_final"
+    compute_fn = staticmethod(_compute_df_final)
+
+class for_submission_loader(base_loader):
+    cache_name = "for_submission"
+    compute_fn = staticmethod(_compute_for_submission)
