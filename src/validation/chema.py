8166715507da7da84bd4fad6ffc8bc12ff46b975
serega-maslo <paulobelov228@gmail.com>
2026-08-23 15:22:38 +0300

    added mlflow logging, improved validation scheme
    Models predictions are now logged in mlflow.  Improved validation scheme by wraping it with mlflow logging and changing format of the result it returns and adding parametres start and window size.

--- src/validation/chema.py
+++ src/validation/chema.py
@@ -1,7 +1,10 @@
 from typing import Protocol
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
+import mlflow
+from sklearn.metrics import root_mean_squared_error
 
 
 class ModelProtocol(Protocol):
@@ -9,26 +12,58 @@ class ModelProtocol(Protocol):
     def predict(self, X: pd.DataFrame) -> np.ndarray: ...
 
 
-def validation_chema(df: pd.DataFrame, model_class: ModelProtocol, loss_function: callable)->pd.Series:
-    results = pd.Series()
+def validation_chema(df: pd.DataFrame, model_class: ModelProtocol, start: int = 0, window_size: int = -1)->pd.DataFrame:
+    results = df[['date_block_num', 'corrected_shop_id', 'corrected_item_id', 'item_cnt_day']].iloc[0:0].copy()
+    results['true_ans'] = None
     max_month = df['date_block_num'].max()
+    if window_size != -1:
+        max_month -= window_size - 1
     columns_to_group = ['corrected_shop_id', 'corrected_item_id']
     train = df.iloc[0:0].copy()
-    for month in range(max_month):
-        train = pd.concat(
-            [train, df[df["date_block_num"] == month]],
-            ignore_index=True,
-        )
+    for month in range(start, max_month):
+        if window_size == -1:
+            train = pd.concat(
+                [train, df[df["date_block_num"] == month]],
+                ignore_index=True,
+            )
+        else:
+            train = df[(df['date_block_num'] >= month) & (df['date_block_num'] < month + window_size)]
+        
         valid = df[df["date_block_num"] == month + 1][['corrected_shop_id', 'corrected_item_id', 'item_cnt_day']]
         valid = (
             valid
             .groupby(columns_to_group, as_index=False)
             .agg(item_cnt_day=("item_cnt_day", "sum"))
         )
+        valid = valid.rename(columns={'item_cnt_day': 'true_ans'})
+        valid['date_block_num'] = month + 1
         model = model_class()
         model.fit(train)
-        true_ans = valid['item_cnt_day']
-        valid = valid.drop(columns=['item_cnt_day'])
         prediction = model.predict(valid)
-        results.loc[month + 1] = loss_function(true_ans, prediction)
+        valid['item_cnt_day'] = prediction
+        results = pd.concat([results, valid], ignore_index=True)
     return results
+
+
+def log_validation(df: pd.DataFrame, model_class, save_path: Path, model_name: str | None = None, start: int = 0, window_size: int = -1):
+    if model_name is None:
+        model_name = model_class.__name__
+
+    with mlflow.start_run(run_name=model_name):
+
+        mlflow.log_param("model_class", model_name)
+        mlflow.log_param("start", start)
+        mlflow.log_param("window_size", window_size)
+
+        results = validation_chema(df=df, model_class=model_class, start=start, window_size=window_size)
+
+        y_true = results["true_ans"].astype(float)
+        y_pred = results["item_cnt_day"].astype(float)
+
+        mlflow.log_metric("rmse", root_mean_squared_error(y_true, y_pred))
+
+        predictions_path = save_path / f"validation_predictions_{model_name}.parquet"
+        results.to_parquet(predictions_path, index=False)
+        mlflow.log_artifact(predictions_path)
+
+        return results
